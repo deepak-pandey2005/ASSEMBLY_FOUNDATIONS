662c4b00064db9cdb816bbce4802d801ec555895
deepak-pandey2005 <deepakpanday.in@gmail.com>
2026-04-17 22:26:59 +0530

    1.hello world in assembly

---
+++ nasm/hello.asm
@@ -0,0 +1,20 @@
+section .data
+
+message: db "hello,world",10
+
+section .text
+global _start
+
+_start:
+    mov rax,1
+    mov rdi,1
+    mov rsi,message
+    mov rdx,13
+    syscall
+    mov rax,60
+    xor rdi,rdi
+    syscall
+
+
+# nasm -felf64 hello.asm
+# ld hello.o
---
+++ readme.md
@@ -0,0 +1,1 @@
+learning Assembly X86.
